9a5f2732edc3a5e3027c1494832d431ea82b623d
Gunwant Jain <mail@wantguns.dev>
2023-02-24 07:11:42 +0530

    drive: add spinner on root page as well
    Signed-off-by: Gunwant Jain <mail@wantguns.dev>

--- src/routes/drive/+page.svelte
+++ src/routes/drive/+page.svelte
@@ -1,3 +1,14 @@
+<script>
+    import { ScaleOut } from 'svelte-loading-spinners';
+    import { navigating } from '$app/stores';
+</script>
+
+{#if $navigating}
+    <div class="w-4/5 flex justify-center items-center text-3xl text-white/20 font-extrabold">
+        <ScaleOut size="120" color="#FFFFFF1a" unit="px" duration="0.75s" />
+    </div>
+{:else}
 <div class="w-4/5 flex justify-center items-center text-3xl text-white/20 font-extrabold">
     <div>&#8592; Choose a Drive</div>
 </div>
+{/if}
